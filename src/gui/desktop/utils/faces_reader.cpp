00193b5a368776c4ea38cabcb1f33fbfbd151f41
Michał Walenciak <Kicer86@gmail.com>
2020-09-30 22:50:27 +0200

    initial implementation

--- src/gui/desktop/utils/faces_reader.cpp
+++ src/gui/desktop/utils/faces_reader.cpp
@@ -1,10 +1,106 @@
 
+#include <QFileInfo>
+
+#include <core/icore_factory_accessor.hpp>
+#include <core/iexif_reader.hpp>
+#include <core/itask_executor.hpp>
+#include <core/oriented_image.hpp>
+#include <core/task_executor_utils.hpp>
+#include <database/ibackend.hpp>
+#include <database/idatabase.hpp>
+#include <face_recognition/face_recognition.hpp>
+
 #include "faces_reader.hpp"
 
 
+template<typename T>
+struct ExecutorTraits<Database::IDatabase, T>
+{
+    static void exec(Database::IDatabase* db, T&& t)
+    {
+        db->exec(std::forward<T>(t));
+    }
+};
+
+
 FacesReader::FacesReader(Database::IDatabase& db, ICoreFactoryAccessor& core)
-    : m_db(db)
-    , m_core(core)
+    : m_core(core)
+    , m_db(db)
 {
 
+}
+
+
+FacesReader::~FacesReader()
+{
+    m_callback_ctrl.invalidate();
+}
+
+
+void FacesReader::get(const Photo::Id& id, const std::function<void (QVector<QRect>)>& callback)
+{
+    auto safe_callback = m_callback_ctrl.make_safe_callback([]{
+
+    });
+
+    auto executor = m_core.getTaskExecutor();
+
+    runOn(executor, safe_callback);
+}
+
+
+void FacesReader::findFaces(const Photo::Id& id)
+{
+    QVector<QRect> result;
+
+    const QString path = pathFor(id);
+    const QFileInfo pathInfo(path);
+    const QString full_path = pathInfo.absoluteFilePath();
+    const OrientedImage image = OrientedImage(m_core.getExifReaderFactory()->get(), full_path);
+
+    const std::vector<QRect> list_of_faces = fetchFacesFromDb(id);
+
+    if (list_of_faces.empty())
+    {
+        FaceRecognition face_recognition(&m_core);
+        const auto faces = face_recognition.fetchFaces(full_path);
+
+        for(const QRect& face: faces)
+            result.append(face);
+    }
+    else
+    {
+        result.reserve(static_cast<int>(list_of_faces.size()));
+
+        std::copy(list_of_faces.cbegin(), list_of_faces.cend(), std::back_inserter(result));
+    }
+}
+
+
+QString FacesReader::pathFor(const Photo::Id& id) const
+{
+    return evaluate<QString(Database::IBackend &)>(&m_db, [id, db = &m_db](Database::IBackend &)
+    {
+        Database::IUtils& db_utils = db->utils();
+        auto photo = db_utils.getPhotoFor(id);
+
+        return photo->getPath();
+    });
+}
+
+
+std::vector<QRect> FacesReader::fetchFacesFromDb(const Photo::Id& id) const
+{
+    return evaluate<std::vector<QRect>(Database::IBackend &)>
+        (&m_db, [id](Database::IBackend& backend)
+    {
+        std::vector<QRect> faces;
+
+        const auto people = backend.peopleInformationAccessor().listPeople(id);
+        for(const auto& person: people)
+            if (person.rect.isValid())
+                faces.push_back(person.rect);
+
+        return faces;
+    });
 }
--- src/gui/desktop/utils/faces_reader.hpp
+++ src/gui/desktop/utils/faces_reader.hpp
@@ -2,6 +2,9 @@
 #ifndef FACESREADER_HPP
 #define FACESREADER_HPP
 
+#include <core/function_wrappers.hpp>
+#include <database/photo_data.hpp>
+
 namespace Database
 {
     struct IDatabase;
@@ -14,11 +17,18 @@ class FacesReader
 {
     public:
         FacesReader(Database::IDatabase &, ICoreFactoryAccessor &);
+        ~FacesReader();
 
+        void get(const Photo::Id &, const std::function<void(QVector<QRect>)> &);
 
     private:
+        safe_callback_ctrl m_callback_ctrl;
         ICoreFactoryAccessor& m_core;
         Database::IDatabase& m_db;
+
+        void findFaces(const Photo::Id &);
+        QString pathFor(const Photo::Id &) const;
+        std::vector<QRect> fetchFacesFromDb(const Photo::Id &) const;
 };
 
 #endif
